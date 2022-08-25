b65c69e21a10a6d1f52f2d377909f8f7e1a25bc0
Harrisson Bernal <harrisson.bernal@gmail.com>
2022-08-25 15:15:27 -0500

    agregando botones de resta y reset al contador

--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -32,12 +32,34 @@ class _CounterScreenState extends State<CounterScreen> {
 
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           FloatingActionButton(
-            child: const Icon(Icons.add_box_outlined),
+            child: const Icon(Icons.add),
             onPressed: () {
               counter++;
+              setState(() {
+                
+              });
+            },
+          ),
+
+          
+          FloatingActionButton(
+            child: const Icon(Icons.all_inclusive),
+            onPressed: () {
+              counter = 0;
+              setState(() {
+                
+              });
+            },
+          ),
+
+       
+          FloatingActionButton(
+            child: const Icon(Icons.horizontal_rule),
+            onPressed: () {
+              counter--;
               setState(() {
                 
               });
