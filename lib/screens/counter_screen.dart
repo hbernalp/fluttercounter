584bda2560b4133ce20ab0c02ab4f9cbedb0e26e
Harrisson Bernal <harrisson.bernal@gmail.com>
2022-08-26 15:00:04 -0500

    Counter Finalizado

--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -10,6 +10,24 @@ class CounterScreen extends StatefulWidget {
 class _CounterScreenState extends State<CounterScreen> {
   int counter = 15;
 
+  //Metodo para el boton de incremento
+  void increase() {
+    counter++;
+    setState(() {});
+  }
+
+  //Metodo  para el boton de decremento
+  void decrease() {
+    counter--;
+    setState(() {});
+  }
+
+  //Metodo  para el boton de reset
+  void resetment() {
+    counter = 0;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     const fontSize30 = TextStyle(fontSize: 30);
@@ -31,42 +49,55 @@ class _CounterScreenState extends State<CounterScreen> {
       ),
 
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          FloatingActionButton(
-            child: const Icon(Icons.add),
-            onPressed: () {
-              counter++;
-              setState(() {
-                
-              });
-            },
-          ),
+      floatingActionButton: CustomFloatingAction(
+        increaseFn: increase,
+        decreaseFn: decrease,
+        resetmentFn: resetment,
+        ),
+    );
+  }
+}
 
-          
-          FloatingActionButton(
-            child: const Icon(Icons.all_inclusive),
-            onPressed: () {
-              counter = 0;
-              setState(() {
-                
-              });
-            },
-          ),
+class CustomFloatingAction extends StatelessWidget {
+  final Function increaseFn;
+  final Function decreaseFn;
+  final Function resetmentFn;
 
-       
-          FloatingActionButton(
-            child: const Icon(Icons.horizontal_rule),
-            onPressed: () {
-              counter--;
-              setState(() {
-                
-              });
-            },
-          ),
-        ],
-      ),
+  const CustomFloatingAction({
+    Key? key,
+    required this.increaseFn,
+    required this.decreaseFn,
+    required this.resetmentFn,
+  }) : super(key: key);
+
+  
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+
+        //Botton de incremento
+        FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () => increaseFn(),
+        ),
+
+        //Botton de reset
+        FloatingActionButton(
+          child: const Icon(Icons.all_inclusive),
+          onPressed: () => resetmentFn(),
+        ),
+
+        //Botton de Decremento
+        FloatingActionButton(
+          child: const Icon(Icons.horizontal_rule),
+          onPressed: () => decreaseFn(),
+        ),
+      ],
     );
   }
 }
